d276039808012eb091712718a65e7d89cf6032ab
Ruiyang Wu <58066925+ywwry66@users.noreply.github.com>
2022-01-25 23:04:51 -0700

    Add switch for qdap and qdap_penal in data analysis wrapper

--- R/data_analysis_wrapper.R
+++ R/data_analysis_wrapper.R
@@ -8,21 +8,35 @@ library(rda)
 library(dsda)
 library(MASS) #for lda and qda
 
-data_analysis <- function(x, y, xnew, ynew, lda=TRUE, qda=TRUE,
-                          dsda=TRUE, sqda=TRUE, rda=TRUE) {
+data_analysis <- function(x, y, xnew, ynew, qdap=TRUE, lda=TRUE, qda=TRUE,
+                          dsda=TRUE, sqda=TRUE, rda=TRUE, qdap_penal=FALSE) {
+    out <- NULL
     ## Our method
-    y[which(y == 2)] <- 0
-    ynew[which(ynew == 2)] <- 0
-    ypred <- qdap(x, as.factor(y), xnew, lambda = 0, iter = 1)$class
-    pe_qdap <- mean(ynew != ypred)
-    out <- data.frame("QDAP" = pe_qdap)
+    pe_qdap <- NULL
+    if (qdap == TRUE) {
+        y[which(y == 2)] <- 0
+        ynew[which(ynew == 2)] <- 0
+        ypred <- qdap(x, as.factor(y), xnew, lambda = 0, iter = 1)$class
+        pe_qdap <- mean(ynew != ypred)
+        out <- c(out, list("QDAP" = pe_qdap))
+    }
+    ## Our method penalization
+    pe_qdap_penal <- NULL
+    if (qdap_penal == TRUE) {
+        ypred <- qdap_cv2(x, as.factor(y), xnew,
+                          num_lambda = 9, lambda_min = 1e-3,
+                          optim = "BFGS", folds = 5, seed = 2020,
+                          margin_num = 50, margin_method = "ratio")$class
+        pe_qdap_penal <- mean(ynew != ypred)
+        out <- c(out, list("QDAP_PENAL" = pe_qdap_penal))
+    }
     ## LDA
     pe_lda <- NULL
     if (lda == TRUE) {
         fit_lda <- lda(x, as.factor(y))
         ypred <- predict(fit_lda, xnew)$class
         pe_lda <- mean(ynew != ypred)
-        out <- data.frame(out, "LDA" = pe_lda)
+        out <- c(out, list("LDA" = pe_lda))
     }
     ## QDA
     pe_qda <- NULL
@@ -30,13 +44,13 @@ data_analysis <- function(x, y, xnew, ynew, lda=TRUE, qda=TRUE,
         fit_qda <- qda(x, as.factor(y))
         ypred <- predict(fit_qda, xnew)$class
         pe_qda <- mean(ynew != ypred)
-        out <- data.frame(out, "QDA" = pe_qda)
+        out <- c(out, list("QDA" = pe_qda))
     }
     ## DSDA
     pe_dsda <- NULL
     if (dsda == TRUE) {
         pe_dsda <- dsda(as.matrix(x), y, as.matrix(xnew), ynew)$error
-        out <- data.frame(out, "DSDA" = pe_dsda)
+        out <- c(out, list("DSDA" = pe_dsda))
     }
     y[which(y == 0)] <- 2
     ynew[which(ynew == 0)] <- 2
@@ -45,7 +59,7 @@ data_analysis <- function(x, y, xnew, ynew, lda=TRUE, qda=TRUE,
     if (sqda == TRUE) {
         invisible(capture.output(
             pe_sqda <- save_restore_seed(apply_DAP, x, y, xnew, ynew)$error))
-        out <- data.frame(out, "DAP" = pe_sqda)
+        out <- c(out, list("DAP" = pe_sqda))
     }
     ## RDA
     pe_rda <- NULL
@@ -59,17 +73,17 @@ data_analysis <- function(x, y, xnew, ynew, lda=TRUE, qda=TRUE,
                        delta = seq(0, 3, len = 10)[par[2]])
         ypred <- predict(fit_rda, t(x), as.factor(y), t(xnew), type = "class")
         pe_rda <- mean(ynew != ypred)
-        out <- data.frame(out, "RDA" = pe_rda)
+        out <- c(out, list("RDA" = pe_rda))
     }
-    return(out)
+    return(as.data.frame(out))
 }
 
 data_analysis_sl <- function(x, y, xnew, ynew, oracle_rule=NULL,
-                             lda=TRUE, qda=TRUE, dsda=TRUE,
-                             sqda=TRUE, rda=TRUE) {
+                             qdap=TRUE, lda=TRUE, qda=TRUE, dsda=TRUE,
+                             sqda=TRUE, rda=TRUE, qdap_penal=TRUE) {
     out <- data_analysis(x = x, y = y, xnew = xnew, ynew = ynew,
-                         lda = lda, qda = qda, dsda = dsda,
-                         sqda = sqda, rda = rda)
+                         qdap = qdap, lda = lda, qda = qda, dsda = dsda,
+                         sqda = sqda, rda = rda, qdap_penal = qdap_penal)
     if (!is.null(oracle_rule)) {
         pe_oracle <- mean(ynew != apply(xnew, 1, oracle_rule))
         out <- data.frame(out, "Oracle" = pe_oracle)
