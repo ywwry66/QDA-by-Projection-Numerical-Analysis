2e08c014055b0164f3bc8fcaa4801738a040fd6f
Ruiyang Wu <58066925+ywwry66@users.noreply.github.com>
2022-02-02 12:31:45 -0700

    Allow generating t-distributed data

--- R/datagen_sl.R
+++ R/datagen_sl.R
@@ -3,9 +3,19 @@
 
 library(MASS)
 
-datagen_sl <- function(mu0, mu1, sigma0, sigma1,
+mvrt <- function(n = 1, mu, sigma, nu) {
+    p <- length(mu)
+    y <- mvrnorm(n, rep(0, p), sigma)
+    u <- rchisq(n, nu)
+    x <- y * matrix(rep(sqrt(u / nu), p), nrow = n) +
+        matrix(rep(mu, n), nrow = n, byrow = TRUE)
+    return(x)
+}
+
+datagen_sl <- function(mu0, mu1, sigma0, sigma1, t_dist = FALSE, nu = t_dist,
                        m=100, ntest=100, n=c(100, 150, 200, 250, 300)) {
     len_n <- length(n)
+    p <- length(mu0)
     ## generating y's
     y <- list()
     ynew <- list()
@@ -16,26 +26,46 @@ datagen_sl <- function(mu0, mu1, sigma0, sigma1,
     ## generating x's
     x <- list()
     xnew <- list()
-    for (i in 1:len_n) {
-        x[[i]] <- list()
-        xnew[[i]] <- list()
-        for (j in 1:m) {
-            x[[i]][[j]] <- rbind(mvrnorm(n[i], mu0, sigma0),
-                                 mvrnorm(n[i], mu1, sigma1))
-            xnew[[i]][[j]] <- rbind(mvrnorm(ntest, mu0, sigma0),
-                                    mvrnorm(ntest, mu1, sigma1))
+    if (t_dist == TRUE) {
+        for (i in 1:len_n) {
+            x[[i]] <- list()
+            xnew[[i]] <- list()
+            for (j in 1:m) {
+                x[[i]][[j]] <- rbind(mvrt(n[i], mu0, sigma0, nu),
+                                     mvrt(n[i], mu1, sigma1, nu))
+                xnew[[i]][[j]] <- rbind(mvrt(ntest, mu0, sigma0, nu),
+                                        mvrt(ntest, mu1, sigma1, nu))
+            }
         }
+        ## generating oracle rules
+        dsigma0 <- det(sigma0)
+        dsigma1 <- det(sigma1)
+        sigma0_inv <- solve(sigma0)
+        sigma1_inv <- solve(sigma1)
+        oracle_rule <- function(xnew)
+            as.numeric((1 + t(xnew - mu0) %*% sigma0_inv %*% (xnew - mu0) / nu) ^ (-(nu + p) / 2) / sqrt(dsigma0) < (1 + t(xnew - mu1) %*% sigma1_inv %*% (xnew - mu1) / nu) ^ (-(nu + p) / 2) / sqrt(dsigma1))
+    } else {
+        for (i in 1:len_n) {
+            x[[i]] <- list()
+            xnew[[i]] <- list()
+            for (j in 1:m) {
+                x[[i]][[j]] <- rbind(mvrnorm(n[i], mu0, sigma0),
+                                     mvrnorm(n[i], mu1, sigma1))
+                xnew[[i]][[j]] <- rbind(mvrnorm(ntest, mu0, sigma0),
+                                        mvrnorm(ntest, mu1, sigma1))
+            }
+        }
+        ## generating oracle rules
+        ldsigma0 <- log(det(sigma0))
+        ldsigma1 <- log(det(sigma1))
+        sigma0_inv <- solve(sigma0)
+        sigma1_inv <- solve(sigma1)
+        oracle_rule <- function(xnew)
+            as.numeric(-ldsigma0 -
+                       t(xnew - mu0) %*% sigma0_inv %*% (xnew - mu0) <
+                       -ldsigma1 -
+                       t(xnew - mu1) %*% sigma1_inv %*% (xnew - mu1))
     }
-    ## generating oracle rules
-    ldsigma0 <- log(det(sigma0))
-    ldsigma1 <- log(det(sigma1))
-    sigma0_inv <- solve(sigma0)
-    sigma1_inv <- solve(sigma1)
-    oracle_rule <- function(xnew)
-        as.numeric(-ldsigma0 -
-                   t(xnew - mu0) %*% sigma0_inv %*% (xnew - mu0) <
-                   -ldsigma1 -
-                   t(xnew - mu1) %*% sigma1_inv %*% (xnew - mu1))
     ## value returned
     return(list(x = x, y = y, xnew = xnew, ynew = ynew,
                 oracle_rule = oracle_rule))
